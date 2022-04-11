6ea00078e24e480df827d12d07c34991adc42b8b
staythere <664770959@qq.com>
2022-04-11 09:26:32 +0800

    Site updated: 2022-04-11 09:26:31

--- js/main.js
+++ js/main.js
@@ -1,147 +1,126 @@
-/* 
- * 2018.8.26 modify by lcc
- * based on the js plugin http://www.htmleaf.com/jQuery/Menu-Navigation/20141212771.html
- * based on other unknown sources...
- * thanks for their open sources!
-*/
-jQuery(document).ready(function($) {
+(function($){
+    // Caption
+    $('.article-entry').each(function(i) {
+        $(this).find('img').filter(function (element) {
+            return $(this).hasClass('');
+        }).each(function() {
+            // add image caption
+            if (this.alt && !(!!$.prototype.justifiedGallery && $(this).parent('.justified-gallery').length)) {
+                $(this).after('<span class="caption">' + this.alt + '</span>');
+            }
 
-	"use strict";
+            if ($(this).parent().prop("tagName") !== 'A') {
+                $(this).wrap('<a href="' + ($(this).attr("data-imgbig") ? $(this).attr("data-imgbig") : this.src) + '" title="' + this.alt + '" class="gallery-item"></a>');
+            }
+        });
+    });
+    if (typeof lightGallery != 'undefined') {
+        var options = {
+            selector: '.gallery-item'
+        };
+        $('.article-entry').each(function(i, entry) {
+            lightGallery(entry, options);
+        });
+        lightGallery($('.article-gallery')[0], options);
+    }
+    if (!!$.prototype.justifiedGallery) {  // if justifiedGallery method is defined
+        var options = {
+            rowHeight: 140,
+            margins: 4,
+            lastRow: 'justify'
+        };
+        $('.justified-gallery').justifiedGallery(options);
+    }
 
-	/* Preloader */
-	var Annie_Preloader = function() {
-		$(window).on("load", function() {
-			// fade out the loading animation
-			$("#status").fadeOut();
+    // Profile card
+    var profileElem = $('#profile');
+    $(document).on('click', function () {
+        profileElem.removeClass('card');
+    }).on('click', '#profile-anchor', function (e) {
+        e.stopPropagation();
+        profileElem.toggleClass('card');
+    }).on('click', '.profile-inner', function (e) {
+        e.stopPropagation();
+    });
 
-			//fade out the white DIV that covers the website
-			$("#preloader").delay(400).fadeOut("slow");
-		});
-	};
+    // To Top
+    var sidebarElem = $('#sidebar'),
+        toTopElem = $('#toTop');
 
-	/* Nav */
-	var Annie_Nav = function() {
-		// browser window scroll (in pixels) after which the "menu" link is shown
-		var offset = 300;
-		var navigationContainer = $('#cd-nav');
-		var mainNavigation = navigationContainer.find('#cd-main-nav ul');
+    (function () {
+        if (!sidebarElem.length) return;
 
-		//hide or show the "menu" link
-		checkMenu();
+        checkDisplayToTop();
+        $(document).on('scroll', function () {
+            checkDisplayToTop();
+        });
 
-		$(window).scroll(function() {
-			checkMenu();
-		});
+        toTopElem.click(function () {
+            $('body, html').animate({ scrollTop: 0 }, 600);
+        });
 
-		//open or close the menu clicking on the bottom "menu" link
-		$('.cd-nav-trigger').on('click', function() {
-			$(this).toggleClass('menu-is-open');
+        var isToTopDisplayed = false;
+        function checkDisplayToTop() {
+            var toTop = (sidebarElem.height() - $(window).height()) + 60;
+            var currentScrollTop = $(document).scrollTop();
+            var needDisplay = (currentScrollTop > toTop) && (currentScrollTop > 0);
 
-			//we need to remove the transitionEnd event handler (we add it when scolling up with the menu open)
-			mainNavigation.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend').toggleClass('is-visible');
-		});
+            if ($(document).width() >= 800) {
+                if (needDisplay) {
+                    if (isToTopDisplayed) return;
+                    toTopElem.fadeIn();
+                    toTopElem.css('left', sidebarElem.offset().left);
+                    isToTopDisplayed = true;
+                } else {
+                    if (!isToTopDisplayed) return;
+                    toTopElem.fadeOut();
+                    isToTopDisplayed = false;
+                }
+            } else {
+                toTopElem.show();
+                toTopElem.css('right', 20);
+            }
+        }
+    })();
 
-		function checkMenu() {
-			if($(window).scrollTop() > offset && !navigationContainer.hasClass('is-fixed')) {
-				navigationContainer.addClass('is-fixed').find('.cd-nav-trigger').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
-					mainNavigation.addClass('has-transitions');
-				});
-			} else if($(window).scrollTop() <= offset) {
+    // Fixed Profile
+    (function () {
+        checkFixedProfile();
+        $(document).on('scroll', function () {
+            checkFixedProfile();
+        });
 
-				//check if the menu is open when scrolling up
-				if(mainNavigation.hasClass('is-visible') && !$('html').hasClass('no-csstransitions')) {
-					//close the menu with animation
-					mainNavigation.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
-						//wait for the menu to be closed and do the rest
-						mainNavigation.removeClass('is-visible is-hidden has-transitions');
-						navigationContainer.removeClass('is-fixed');
-						$('.cd-nav-trigger').removeClass('menu-is-open');
-					});
+        var isFixedProfile = false;
+        function checkFixedProfile() {
+            if (!profileElem.is('.profile-fixed')) return;
+            if ($(document).width() < 800) return;
 
-					//check if the menu is open when scrolling up - fallback if transitions are not supported
-				} else if(mainNavigation.hasClass('is-visible') && $('html').hasClass('no-csstransitions')) {
-					mainNavigation.removeClass('is-visible has-transitions');
-					navigationContainer.removeClass('is-fixed');
-					$('.cd-nav-trigger').removeClass('menu-is-open');
+            var currentScrollTop = $(document).scrollTop();
+            var profileInnerElem = $('#profile .profile-inner');
+            var needFixed = currentScrollTop >= profileElem.offset().top + profileElem.outerHeight(true);
 
-					//scrolling up with menu closed
-				} else {
-					navigationContainer.removeClass('is-fixed');
-					mainNavigation.removeClass('has-transitions');
-				}
-			}
-		}
-	};
+            if (needFixed) {
+                if (isFixedProfile) return;
 
-	/* Random bg-img for header*/
-	var Annie_Random = function() {
-		//generate a random img that pre_name 'from 0 to 110'
-		var random_bg = Math.floor(Math.random() * 109 + 1);
+                profileInnerElem.css('position', 'fixed')
+                    .css('width', profileElem.innerWidth() + 'px')
+                    .css('top', '0');
 
-		//var bg = 'url(/img/random/' + random_bg + '.jpg)';
-		var bg = 'url(/img/random/' + random_bg + '.jpg)';
+                // css animation fade-in
+                profileInnerElem.css('animation', '');
+                profileInnerElem.addClass('anim-fade-in');
+                isFixedProfile = true;
+            } else {
+                if (!isFixedProfile) return;
 
-		$("#header-bg-2").css("background-image", bg);
-	};
+                profileInnerElem.css('position', '')
+                    .css('width', '')
+                    .css('top', '');
 
-	/* ToTop */
-	var Annie_ToTop = function() {
-		var upperLimit = 500;
+                profileInnerElem.css('animation', 'none');
+                isFixedProfile = false;
+            }
+        }
+    })();
 
-		// Our scroll link element
-		var scrollElem = $('#totop');
-
-		// Scroll to top speed
-		var scrollSpeed = 500;
-
-		scrollElem.hide();
-
-		$(window).scroll(function() {
-			var scrollTop = $(document).scrollTop();
-
-			if(scrollTop > upperLimit) {
-				$(scrollElem).stop().fadeTo(300, 1);
-			} else {
-				$(scrollElem).stop().fadeTo(300, 0);
-			}
-		});
-
-		$(scrollElem).click(function() {
-			$('html, body').animate({
-				scrollTop: 0
-			}, scrollSpeed);
-			return false;
-		});
-	};
-
-	/* Show Comment */
-	var Annie_Comment = function() {
-		function Show_Hidden(obj) {
-			var obj = $('#annie-comment-container');
-		}
-
-		var obutton = document.getElementById("annie-comment-button");
-		var odiv = document.getElementById("annie-comment-container");
-		//var obutton = $('#annie-comment-button');
-		//var odiv = $('#annie-comment-container');	
-		if('obutton') {
-			obutton.onclick = function() {
-				Show_Hidden(odiv);
-				$("#annie-comment-button").css("display", 'none');
-				return false;
-			}
-		}
-	};
-
-	/* other js function */
-	/* ... */
-
-	/* Initialize */
-	(function Annie_Init() {
-		Annie_Preloader();
-		Annie_Nav();
-		//Annie_Random();
-		Annie_ToTop();
-		//Annie_Comment();
-	})();
-});
+})(jQuery);
